5b665641a9967d1e8ed50b8335570443485c1d5b
kimyongkuk <k0603156@gmail.com>
2019-12-16 17:39:21 +0900

    reset

--- server/scripts/docker_mount.sh
+++
@@ -1,9 +0,0 @@
-# docker rm -f $(docker ps -a | grep traveler | awk "{print \$1}")
-
-# 기존 컨테이너는 제거
-docker rm -f traveler-container
-
-# 컨테이너실행
-docker run --name traveler-container -itd --restart no -p 8000:8000 traveler
-
-# docker logos
