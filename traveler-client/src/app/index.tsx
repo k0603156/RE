bf8f13b3ad1eb1e7f20a7f00c9c8b336f6154e82
kimyongkuk <k0603156@gmail.com>
2019-12-25 02:03:38 +0900

    styled-components 적용 build 파일에 displayName false

--- traveler-client/src/app/index.tsx
+++ traveler-client/src/app/index.tsx
@@ -2,44 +2,55 @@ import React from "react";
 import _ from "lodash";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { PrivateRoutes, PublicRoutes, SessionRoutes } from "routes";
+import styled, { ThemeProvider } from "styled-components";
 import PrivateLayout from "./privateLayout";
 import PublicLayout from "./publicLayout";
+import Theme from "styles/Theme";
 import { user } from "types/type";
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: ${props => props.theme.maxWidth};
+  width: 100%;
+`;
 function App() {
   const user: user = {
     userName: "kimyongkuk",
     isLogged: true
   };
   return (
-    <BrowserRouter>
-      <Switch>
-        {_.map(PublicRoutes, (route, key) => {
-          const { component, path } = route;
-          return (
-            <Route
-              exact
-              path={path}
-              key={key}
-              render={route =>
-                user.isLogged ? (
-                  <PrivateLayout
-                    component={component}
-                    route={route}
-                    user={user}
-                  />
-                ) : (
-                  <PublicLayout
-                    component={component}
-                    route={route}
-                    user={user}
-                  />
-                )
-              }
-            />
-          );
-        })}
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={Theme}>
+      <BrowserRouter>
+        <Wrapper>
+          <Switch>
+            {_.map(PublicRoutes, (route, key) => {
+              const { component, path } = route;
+              return (
+                <Route
+                  exact
+                  path={path}
+                  key={key}
+                  render={route =>
+                    user.isLogged ? (
+                      <PrivateLayout
+                        component={component}
+                        route={route}
+                        user={user}
+                      />
+                    ) : (
+                      <PublicLayout
+                        component={component}
+                        route={route}
+                        user={user}
+                      />
+                    )
+                  }
+                />
+              );
+            })}
+          </Switch>
+        </Wrapper>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
---
+++ traveler-client/src/styles/Theme.js
@@ -0,0 +1,23 @@
+const BOX_BORDER = "border: 1px solid #e6e6e6";
+const BORDER_RADIUS = "border-radius: 4px";
+export default {
+  maxWidth: "935px",
+  bgColor: "#FAFAFA",
+  blackColor: "#262626",
+  blueColor: "#3897f0",
+  redColor: "#ED4956",
+  lightGreyColor: "#c7c7c7",
+  darkGreyColor: "#999",
+  darkBlueColor: "#003569",
+  whiteBox: `
+    ${BORDER_RADIUS};
+    ${BOX_BORDER};
+    background-color:white;
+`,
+
+  trendBgColor: "#F2F2F2",
+  trendPink: "#BF545D",
+  trendDarkBlue: "#0F1926",
+  trendBeige: "#F2BC79",
+  trendRed: "#8C0303"
+};
