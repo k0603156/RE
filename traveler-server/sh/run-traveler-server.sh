3677fa39da83516c39e222ea128a03ec4ee4b379
kimyongkuk <k0603156@gmail.com>
2019-12-29 18:31:26 +0900

    docker, mysql, docker shellscript,  express run script #4

---
+++ traveler-server/sh/run-traveler-server.sh
@@ -0,0 +1,11 @@
+# 네트워크생성
+docker network create traveler_express_net
+# 이미지 빌드
+docker build -t traveler_image -f Dockerfile .
+# 동일한 이름의 이전 컨테이너 제거
+docker rm -f  $(docker ps -a | grep "traveler_container" | awk "{print \$1}")
+# 지난 이미지 삭제
+docker rmi -f $(docker images | grep "<none" | awk "{print \$3}")
+# 컨테이너 생성
+docker run -itd --name traveler_container -p 80:80 --network traveler_express_net --restart unless-stopped traveler_image
+
