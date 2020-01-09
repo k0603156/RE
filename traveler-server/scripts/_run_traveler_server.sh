d35d6616b8b572229fd4129f71fe46cacd3e9c5e
kimyongkuk <k0603156@gmail.com>
2020-01-10 03:18:59 +0900

    컨테이너 실행 스크립트 수정

--- traveler-server/scripts/_run_traveler_server.sh
+++ traveler-server/scripts/_run_traveler_server.sh
@@ -7,5 +7,6 @@ docker rm -f  $(docker ps -a | grep "traveler_container" | awk "{print \$1}")
 # 지난 이미지 삭제
 docker rmi -f $(docker images | grep "<none" | awk "{print \$3}")
 # 컨테이너 생성
-docker run -itd --name traveler_container -p 80:80 --network traveler_express_net --restart unless-stopped traveler_image
+# docker run -itd --name traveler_container -p 80:80 --network traveler_express_net --restart unless-stopped traveler_image
+docker run -itd --name traveler_container -p 80:80 --restart unless-stopped traveler_image
 
