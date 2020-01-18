eb7662a3f16e4d42cf5451771bb416637fa8edc0
kimyongkuk <k0603156@gmail.com>
2020-01-18 17:23:36 +0900

    create src/*  node_modules/.bin/tsc --init

---
+++ traveler-client/src/CLIENT/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+import App from "./components/App";
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(<App />, rootElement);
