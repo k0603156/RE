3d510fafcc05d6c87283e8090f617f67e19fe027
kimyongkuk <k0603156@gmail.com>
2020-01-18 18:32:04 +0900

    Add pages

--- traveler-client/src/CLIENT/components/App.tsx
+++ traveler-client/src/CLIENT/components/App.tsx
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Helmet from "react-helmet";
+
+import Home from "../pages/Home";
+import News from "../pages/News";
+import Header from "./Header";
+import Footer from "./Footer";
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <Helmet>
+          <title>Traveler</title>
+        </Helmet>
+        <Route path="/" render={() => <Header />} />
+        <Switch>
+          <Route exact path="/" render={() => <Home />} />
+          <Route path="/news" render={() => <News />} />
+        </Switch>
+        <Footer />
+      </div>
+    );
   }
 }
 
