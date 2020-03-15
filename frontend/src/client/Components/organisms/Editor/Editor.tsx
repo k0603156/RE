4402eb66adc7a971377b0b96379b7cb75eb1cdb3
kimyongkuk <k0603156@gmail.com>
2020-03-15 20:08:41 +0900

    Add textEditor toolbar

--- frontend/src/client/Components/organisms/Editor/Editor.tsx
+++ frontend/src/client/Components/organisms/Editor/Editor.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { createEditor, Node } from "slate";
-import { Slate, Editable, withReact } from "slate-react";
+import { createEditor, Node, Editor as BaseEditor } from "slate";
+import { Slate, Editable, withReact, useSlate } from "slate-react";
 import styled from "styled-components";
 import { Mx_Width } from "@Client/Styles/Device";
 
@@ -9,16 +9,45 @@ interface IProps {
   value: Array<Node>;
 }
 
+const Toolbar = styled.div`
+  position: relative;
+  padding: 1px 18px 17px;
+  margin: 0 -20px;
+  border-bottom: 2px solid #eee;
+  margin-bottom: 20px;
+  & > * {
+    display: inline-block;
+  }
+  & > * + * {
+    margin-left: 15px;
+  }
+`;
+const isMarkActive = (editor: any, format: any) => {
+  const marks = BaseEditor.marks(editor);
+  return marks ? marks[format] === true : false;
+};
+
+const MarkButton = (format: any) => {
+  const editor = useSlate();
+  return (
+    <button
+      disabled={isMarkActive(editor, format)}
+      onMouseDown={event => {
+        event.preventDefault();
+      }}
+    >
+      <span>I</span>
+    </button>
+  );
+};
+
 const Editor = (props: IProps) => {
   const editor = useMemo(() => withReact(createEditor()), []);
-  const [values, setValue] = useState<Array<Node>>([
-    {
-      type: "paragraph",
-      children: [{ text: "A line of text in a paragraph." }]
-    }
-  ]);
   return (
     <Slate editor={editor} value={props.value} onChange={props.onChange}>
+      <Toolbar>
+        <MarkButton format="bold" />
+      </Toolbar>
       <Editable
         onKeyDown={event => {
           console.log(event.key);
