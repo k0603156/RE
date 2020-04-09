3b14252d82e48723cbc0b8ea6d22aec0929624f6
kimyongkuk <k0603156@gmail.com>
2020-04-09 13:11:29 +0900

    Use Storybook

---
+++ frontend/.storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  stories: ["../src/**/*.stories.tsx"],
+  addons: [
+    "@storybook/react",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+  ],
+  webpackFinal: async (config) => {
+    // do mutation to the config
+
+    return config;
+  },
+};
---
+++ frontend/src/client/index.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default {
+  title: "Welcome",
+};
+
+export const Welcome = () => {
+  return <div>ReadMe</div>;
+};
+
+Welcome.story = {
+  name: "ReadMe",
+};
