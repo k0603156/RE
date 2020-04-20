46398512810e694a801c049c580a4a1fe8744959
kimyongkuk <k0603156@gmail.com>
2020-04-20 22:25:47 +0900

    스키마이름 변경

--- database/sh/init.sql
+++ database/sh/init.sql
@@ -13,13 +13,13 @@
 -- Create User
 CREATE USER dev identified by '{kimyongkukdevsqlpw}';
 -- DB 생성
-CREATE DATABASE symphonia_test;
+CREATE DATABASE re_test;
 -- DB =>utf-8
-ALTER DATABASE symphonia_test CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER DATABASE re_test CHARACTER SET utf8 COLLATE utf8_general_ci;
 -- transactionLevel read committed
 SET
   GLOBAL TRANSACTION ISOLATION LEVEL READ COMMITTED;
--- 모든 symphonia_test 모든 테이블 접근권한 with SuperUser
-  GRANT ALL PRIVILEGES ON symphonia_test.* TO dev WITH GRANT OPTION;
+-- 모든 re_test 모든 테이블 접근권한 with SuperUser
+  GRANT ALL PRIVILEGES ON re_test.* TO dev WITH GRANT OPTION;
 -- 적용
   flush privileges;
