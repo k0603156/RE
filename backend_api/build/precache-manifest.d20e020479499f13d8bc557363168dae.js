2eb9b9dd2927cf563024dc4378fabc612d664ce6
kimyongkuk <k0603156@gmail.com>
2020-04-21 01:07:29 +0900

    client build

---
+++ backend_api/build/precache-manifest.d20e020479499f13d8bc557363168dae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9867a40281290c2c39d04971c3e7037e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74d2b39accdf5aed1298",
+    "url": "/static/js/2.518c9ae1.chunk.js"
+  },
+  {
+    "revision": "30ee3e6dbe011f4523b208589e371251",
+    "url": "/static/js/2.518c9ae1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2fecfb533a2a8142fe5",
+    "url": "/static/js/main.99ae2363.chunk.js"
+  },
+  {
+    "revision": "e961b1fa1300280defc8",
+    "url": "/static/js/runtime-main.10647e86.js"
+  }
+]);
